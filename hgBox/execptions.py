6bc6529cbc794b49f96e78d22da8e5a62be500ab
huahuaqin <huahuaqin@yeah.net>
2018-11-22 23:38:25 +0800

    增加异常处理信信息

--- hgBox/execptions.py
+++ hgBox/execptions.py
@@ -10,8 +10,12 @@ class JiraException(Exception):
         self.e = e
 
     def __str__(self):
-        err_msg = u''
+        err_msg = str('未知异常')
         if isinstance(self.e, ConnectTimeout):
             err_msg = str('登陆JIRA超时;请确认地址正确/网络正常！')
+        if isinstance(self.e, JIRAError):
+            if self.e.status_code == 401:
+                err_msg = str('JIRA用户名/密码错误！')
         return err_msg
+
 
